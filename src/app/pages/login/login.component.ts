aa70c6e6776d39db1522bda013b2f64bb28af2d6
Everton Mateus Fernandes <everton.mfernandes@gmail.com>
2020-10-09 08:16:32 -0300

    Loading login e mensagem de erro usuario e senha invalidos

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthDto } from 'src/app/models/authDto';
+import { PoNotificationService } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-login',
@@ -14,35 +15,48 @@ export class LoginComponent implements OnInit {
 
   auth: AuthDto;
   form: FormGroup;
+  isHideLoading: boolean;
 
   constructor(
     public formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private poNotification: PoNotificationService
   ) {
-    this.auth =  {
-      email : 'everton.mfernandes@gmail.com',
-      senha : '1234',
+    this.auth = {
+      email: 'everton.mfernandes@gmail.com',
+      senha: '1234',
     };
     this.form = this.formBuilder.group({
       email: ['everton.mfernandes@gmail.com', [Validators.required]],
-      senha: ['1234' , [Validators.required, Validators.minLength(4)]],
+      senha: ['1234', [Validators.required, Validators.minLength(4)]],
     });
+    this.isHideLoading = true;
   }
   ngOnInit(): void {
   }
 
 
   logar(): void {
+    this.isHideLoading = false;
     this.authService.authenticate(this.form.value)
       .subscribe(response => {
         this.authService.successfulLogin(response.headers.get('Authorization'));
         this.router.navigate(['']);
+        this.isHideLoading = true;
       },
         error => {
+          let message = 'Erro ao fazer requisição ao servidor.';
+          this.isHideLoading = true;
+          if (error.error) {
+            message = JSON.parse(error.error).message;
+          } else if (error.status === 401) {
+            message = 'Usuário e senha inválidos';
+          }
+          this.poNotification.error(message);
           this.authService.logout();
           console.log(error);
-    });
+        });
   }
 
 }
